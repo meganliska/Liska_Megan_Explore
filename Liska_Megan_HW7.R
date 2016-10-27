ecb9177060dc46513e13181468f782277d8a2b7a
meganliska <ml1623@georgetown.edu>
2016-10-27 17:39:35 -0400

    fixed one need to fix all the others

--- Liska_Megan_HW7.R
+++ Liska_Megan_HW7.R
@@ -13,9 +13,12 @@ freqtable <- function(x){
   
   #Returns:
   #Frequency table 
-  factorcols <- x[sapply(x, is.factor)] #uses sapply to grab the columns of 
-  #dataframe x which are factor columns (finds factor columns using is.factor)
-  print(summary(factorcols))
+  factorcols <-sapply(data, function (x) (is.factor(x) || is.logical(x))) #uses sapply to grab the columns of 
+  #dataframe x which are factor columns and logic columns (finds factor columns using is.factor)
+  catdata <- x[,factorcols] #takes just the categorical part of the data
+  ftable <- sapply(data, function(x) summary(x)) #uses sapply and function(x) to apply summary to
+  #each of the columns 
+  return(ftable)
 }
 freqtable(diamonds) #test case 
 
@@ -48,11 +51,12 @@ rsquare <- function(x){
   #Returns:
   #a new dataframe 
   
-  data.numerics <- data[sapply(data, is.numeric)]#create a variable with just the numeric columns
+  data.numerics <- data[sapply(data, is.numeric)] #create a variable with just the numeric columns
   
-  combos <- combn(colnames(data.numerics), 2)#use combn to get all the pairs of 2 columns
+  combos <- combn(colnames(data.numerics), 2) #use combn to get all the pairs of 2 columns
   
-  interactions <- paste(combos[1,], combos[2,], sep = '~') #use the paste to what
+  interactions <- paste(combos[1,], combos[2,], sep = '~') #use the paste to make sure names are 
+  #seperated by a ~
   
   #now we use lm (linear regression) and $r.squared to extract the r squared value from the regression 
   coeff.det <- unname(sapply(interactions, function(x) summary(lm(x, data = data.numerics))$r.squared))
@@ -201,27 +205,33 @@ numeric_plot <- function(data, plot_switch, binVec) {
     }
     
     if(plot_switch == "grid"){    #2nd case when switch is "grid"
+      m <- lapply(data[name], mean)
+      
       count_plots <- list()       #Create list to store histogram subplots of each bin size
       density_plots <- list()           #Create list to store the density histograms subplots of each bin size
+      
       if(missing(binVec)){              #case when the vector is null and prints histogram using the default bins 30
         print(ggplot(data, aes_string(name), color = "blue") + geom_histogram(fill="blue")+ 
-                labs(title= "default bins"))
+                labs(title= "default bins")) + geom_vline(xintercept = m[[1]], colour="red") 
         
         print(ggplot(data, aes_string(name), color = "blue") + 
-                geom_histogram(aes(y= ..density..), fill="blue")+ labs(title= "default bins"))
+                geom_histogram(aes(y= ..density..), fill="blue")+ labs(title= "default bins")) + 
+          geom_vline(xintercept = m[[1]], colour="red") 
         #prints out two historgrams using ggplot 
       }else{                            
         for(i in 1:length(binVec)) {    #loop through each bin size in binVec and create a subplot
           k <- ggplot(data, aes_string(name), color = "blue") + 
-            geom_histogram(fill="blue", bins = binVec[i])+ labs(title= paste(binVec[i], "bins"))
+            geom_histogram(fill="blue", bins = binVec[i])+ labs(title= paste(binVec[i], "bins"))+ 
+            geom_vline(xintercept = m[[1]], colour="red") 
           count_plots[[i]] <- k           #Puts subplots in a list 
         }
         multiplot(plotlist = count_plots, cols = 2)  #uses multiplot to plot our graphs   
         #we repeat the process expcept for the second histogram 
+        
         for(i in 1:length(binVec)) {   
           k <- ggplot(data, aes_string(name), color = "blue") + 
             geom_histogram(aes(y= ..density..), fill="blue", bins = binVec[i])+ 
-            labs(title= paste(binVec[i], "bins"))
+            labs(title= paste(binVec[i], "bins"))+ geom_vline(xintercept = m[[1]], colour="red") 
           density_plots[[i]] <- k       
         }
         multiplot(plotlist = density_plots, cols = 2)
@@ -246,17 +256,18 @@ cata_binary_plot <-function(data, plot_switch){
   cata_binary_data <- data[cata_binary]     #extract binary and categorical columns 
   
   if(plot_switch == "on" || plot_switch == "grid") {      #if plot switch is on or grid we want our graphs
+    
     for(name in colnames(cata_binary_data)) {         #loop through the sorted dataframe and plot bar graphs for each column
-      j <- ggplot(cata_binary_data, aes_string(name), color = "grey") + geom_bar(fill="grey")
+      cat_plot <- ggplot(cata_binary_data, aes_string(name), color = "grey") + geom_bar(fill="grey")
       #use ggplot to make our graph and print it 
-      print(j)
+      print(cat_plot)
     }
   }
 }
 
 #Now finally we will take all the functions that we have made and combine them into our explore 
 #function as was directed in the homework 
-explore <-function(data,plot_switch,threshold,binVec){
+explore <-function(data, plot_switch, threshold, binVec){
   #explore takes four parameters: a dataframe, a plot switch which accepts the values: off, on or grid
   #a threshold cut-off value between 0 and 1 for correlations, and an optional vector that contains one or
   #more integers that represent the numbers of bins to use for a historgram. If the vector is not provided
@@ -270,13 +281,14 @@ explore <-function(data,plot_switch,threshold,binVec){
   
   #Returns:
   #An R list 
+  
   outputdata <- freqtable(data)
   datasummary <- colsummary(data)
   rtable <- rsquare(data)
   corCoef(data)
-  Pearsontable <- abs_pearson(newdf)
-  outputplot <- numeric_plot()
-  outputplot2 <- cata_binary_plot()
+  Pearsontable <- abs_pearson(newdf,threshold)
+  outputplot <- numeric_plot(data,plot_switch,binVec)
+  outputplot2 <- cata_binary_plot(data,Plot_switch,binVec)
   
   #we want out output to be an R list so we take the desired variables here and make them into 
   #a new list which is what the function will return 
